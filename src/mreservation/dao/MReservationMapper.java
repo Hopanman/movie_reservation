4ebf75124d29c545a21de1bf4df315822d8b9d5a
Hopanman <soredoom9@naver.com>
2019-12-17 16:32:00 +0900

    vo 추가

--- src/mreservation/dao/MReservationMapper.java
+++ src/mreservation/dao/MReservationMapper.java
@@ -1,10 +1,12 @@
 package mreservation.dao;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import mreservation.vo.*;
 
 public interface MReservationMapper {
 	
 	ArrayList<Movie> movieList();
 	ArrayList<Screening> screeningList();
+	AuditoriumSize getAuditoriumSize(HashMap<String, String> map);
 }
--- src/mreservation/vo/Screening.java
+++ src/mreservation/vo/Screening.java
@@ -4,18 +4,18 @@ public class Screening {
 	private String screening_start;
 	private String screening_end;
 	private String movie_title;
-	private String auditorium_name;
+	private int auditorium_id;
 	
 	public Screening() {
 		
 	}
 
-	public Screening(String screening_start, String screening_end, String movie_title, String auditorium_name) {
+	public Screening(String screening_start, String screening_end, String movie_title, int auditorium_id) {
 		super();
 		this.screening_start = screening_start;
 		this.screening_end = screening_end;
 		this.movie_title = movie_title;
-		this.auditorium_name = auditorium_name;
+		this.auditorium_id = auditorium_id;
 	}
 
 	public String getScreening_start() {
@@ -42,18 +42,18 @@ public class Screening {
 		this.movie_title = movie_title;
 	}
 
-	public String getAuditorium_name() {
-		return auditorium_name;
+	public int getAuditorium_id() {
+		return auditorium_id;
 	}
 
-	public void setAuditorium_name(String auditorium_name) {
-		this.auditorium_name = auditorium_name;
+	public void setAuditorium_id(int auditorium_id) {
+		this.auditorium_id = auditorium_id;
 	}
 
 	@Override
 	public String toString() {
 		return "Screening [screening_start=" + screening_start + ", screening_end=" + screening_end + ", movie_title="
-				+ movie_title + ", auditorium_name=" + auditorium_name + "]";
+				+ movie_title + ", auditorium_id=" + auditorium_id + "]";
 	}
 	
 	
